707a7dab971179b71b4df385f986c1d1756c777b
Jose Antonio <jaborfer@gmail.com>
2024-11-21 09:49:07 +0100

    final

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
 import os
 #importaciones necesarias
+from langchain.chains import ConversationalRetrievalChain
+
 from dotenv import load_dotenv, find_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+
 
 #Cargar el archivo .env
 _ = load_dotenv(find_dotenv())
 #Configuramos el modelo de OpenAI
 openai_api_key = os.environ["OPENAI_API_KEY"]
-chatModel = ChatOpenAI(model="gpt-4o-mini")
+llm = OpenAI(temperature=0)
 
 #Cargamos el archivo PDF
-document = ""
 loader = PyPDFLoader('./Documentos/BOE-A-2024-24098.pdf')
 loaded_data = loader.load()
 
-
 print ("Número de páginas cargadas: ", len(loaded_data))
-for page in loaded_data:
-    document += page.page_content
-    #print(page.page_content)
-    #print("\n----------\n")
+
 
 #Cortamos en trocitos la información (SPLIT)
 
 text_splitter = CharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=100,
+    chunk_size=500,
+    chunk_overlap=50,
     length_function=len,
     is_separator_regex=False,
 )
@@ -47,8 +45,13 @@ vector_db = Chroma.from_documents(chunks, embeddings_model)
 #Preguntamos a la IA
 question = "¿Qué dice el artículo 2?"
 
-response = vector_db.similarity_search(question)
+qa_chain = ConversationalRetrievalChain.from_llm(llm, vector_db.as_retriever())
 
+history = []
+response = qa_chain.invoke({"question": question, "chat_history": history})
+history.append((question, response["answer"]))
 print("\n----------\n")
-print("Respuesta:", response)
+print("Pregunta 1:", question)
+print("\n----------\n")
+print("Respuesta:", response["answer"])
 print("\n----------\n")
